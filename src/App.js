891d6c974dfd5a83800e03f65f7bdb14e8e98650
chrisfromhood <kristijan@cogentdeciphering.com>
2021-10-25 15:39:17 +0200

    inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <div className="row">
+
+        </div>
+      </header>
+    </div>
+
+  );
+}
+export default App;
