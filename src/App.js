7b8ec41947d6e9b3a4c1a2f3f4f98cd12d84dfca
chrisfromhood <kristijan@cogentdeciphering.com>
2021-10-25 16:21:39 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 function App() {
   return (
-    <div>
-      <header className="header">
-        <div className="row">
-          novi red
+    <main className="wrapper">
+      <header className="global-header">
+        <div className="header-top clearfix">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-auto col-sm12 align-self-center">
+                <div className="branding">
+                  <a className="navbar-brand" href="/">
+                    <img src="/src/assets/img/local-united-header-logow.png" alt="logo" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
-    </div>
+    </main>
 
   );
 }
