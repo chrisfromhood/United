985962c7d084a3b947da0eda120d4a761bf5374c
chrisfromhood <kristijan@cogentdeciphering.com>
2021-10-26 10:17:51 +0200

    Starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-
 function App() {
   return (
-    <main className="wrapper">
-      <header className="global-header">
-        <div className="header-top clearfix">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-auto col-sm12 align-self-center">
-                <div className="branding">
-                  <a className="navbar-brand" href="/">
-                    <img src="/src/assets/img/local-united-header-logow.png" alt="logo" />
-                  </a>
-                </div>
+
+    <header className="global-header">
+
+      <div className="header-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-auto col-sm-12 align-self-center">
+              <div className="branding">
+                <a className="branding-logo" href="/">
+                  <img src="https://localunitedservices.com/wp-content/themes/san-diego-home-services/img/local-united-header-logow.png" alt="United Plumbing, Heating, Air and Electric Logo" class="header-logo" />
+                </a>
+                {/* <div className="nav-toggler">
+                  <button type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false" aria-controls="navbarResponsive" class=""> <span class="menu">MENU</span> <span class="menu-icon fas fa-bars"></span> </button>
+                </div> */}
               </div>
             </div>
+            <button type="button" class="btn btn-primary btn-lg">EMERGENCY SERVICE</button>
           </div>
         </div>
-      </header>
-    </main>
+      </div>
+      <div className="header-bottom">
+        <div className="container">
+          <div className="navbar-toggleable navbar-expand-lg" id="navbarResponsive">
+            <nav className="navbar">
+              <ul className="nav navbar-nav mx-auto">
+                <li class="dropdown trigger-fix">
+                  <a href="＃" class="drop-toggle" data-toggle="dropdown"> Services
+                    <span class="fas fa-chevron-down"></span> </a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Plumbing</a></li>
+                    <li><a href="#">Heating</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+        </div>
+      </div>
+
+    </header>
+
 
   );
 }
