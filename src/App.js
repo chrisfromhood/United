649e8129fe905091a1338efa34f145553113ddef
chrisfromhood <kristijan@cogentdeciphering.com>
2021-10-27 09:09:10 +0200

    badges

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ function App() {
           </div>
         </div>
       </section>
-
+      {/* CARDS SECTION */}
       <section className="services-info">
         <div className="container">
           <div className="box-wrapper col-12">
@@ -160,6 +160,25 @@ function App() {
         </div>
       </section>
 
+      <section className="why-us">
+        <div className="container">
+        </div>
+      </section>
+
+      <section className="badges-section">
+        <div className="container">
+          <div className="row justify-content-center text-center">
+            <div className="col badges align-self-center text-center">
+              <img data-src="https://cdn.searchkings.ca/img/badges/bbb-a-plus-v-416c877c89.png" alt="Better Business Bureau" src="https://cdn.searchkings.ca/img/badges/bbb-a-plus-v-416c877c89.png" data-was-processed="true" />
+              <img data-src="https://cdn.searchkings.ca/img/badges/home-advisor-1ca28017c0.png" alt="Home Advisor" src="https://cdn.searchkings.ca/img/badges/home-advisor-1ca28017c0.png" data-was-processed="true" />
+              <img data-src="https://cdn.searchkings.ca/img/badges/super-service-c37e073c9c.png" alt="" src="https://cdn.searchkings.ca/img/badges/super-service-c37e073c9c.png" data-was-processed="true" />
+              <img data-src="https://cdn.searchkings.ca/img/badges/facebook-reviews-f8f17ebab9.png" alt="" src="https://cdn.searchkings.ca/img/badges/facebook-reviews-f8f17ebab9.png" data-was-processed="true" />
+              <img data-src="https://cdn.searchkings.ca/img/badges/trane-comfort-specialist-shield-539c7f9f8b.png" alt="Trane Comfort Specialist" src="https://cdn.searchkings.ca/img/badges/trane-comfort-specialist-shield-539c7f9f8b.png" data-was-processed="true" />
+            </div>
+
+          </div>
+        </div>
+      </section>
 
 
     </div>
