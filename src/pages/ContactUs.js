1fe3dc6e04f1ace1825b472d9a6da7b073565f03
chrisfromhood <kristijan@cogentdeciphering.com>
2021-10-29 17:07:09 +0200

    added form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -60,9 +60,49 @@ function ContactUs() {
                                     <div className="s-box-wrapper">
                                         <div className="s-box-large">
                                             <div className="h4">CONNECT WITH SPECIALIST</div>
-                                            <div className="form-message">
 
+                                            <div className="form-message">
+                                                <form>
+                                                    <div class="form-row">
+                                                        <div class="col">
+                                                            <div className="field-name">
+                                                                <label for="validationDefault01">First name</label>
+                                                                <input type="text" class="form-control " id="validationDefault01" />
+                                                                <div class="valid-feedback">Looks good!</div>
+                                                            </div>
+                                                            <div className="field-last-name">
+                                                                <label for="validationDefault01">Last name</label>
+                                                                <input type="text" class="form-control " id="validationDefault01" />
+                                                                <div class="valid-feedback">Looks good!</div>
+                                                            </div>
+                                                            <div class="field-phone">
+                                                                <label for="validationDefault01">Phone</label>
+                                                                <input type="text" class="form-control" id="validationDefault01" aria-describedby="validationServer05Feedback" required />
+                                                                <div id="validationServer05Feedback" class="invalid-feedback">
+                                                                    Please provide a valid phone number.
+                                                                </div>
+                                                            </div>
+                                                            <div class="field-zip">
+                                                                <label for="validationDefault01">Zip</label>
+                                                                <input type="text" class="form-control" id="validationDefault01" aria-describedby="validationServer05Feedback" required />
+                                                                <div id="validationServer05Feedback" class="invalid-feedback">
+                                                                    Please provide a valid zip.
+                                                                </div>
+                                                            </div>
+                                                            <div class="mb-3">
+                                                                <label for="validationTextarea">Textarea</label>
+                                                                <textarea class="form-control" id="validationTextarea" placeholder="Required example textarea" required=""></textarea>
+                                                                <div class="invalid-feedback">
+                                                                    How can we help?
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <button class="btn btn-primary" type="submit">Submit</button>
+                                                </form>
                                             </div>
+
                                         </div>
                                         <div className="review-box"></div>
                                         <div className="s-box-small text-center"></div>
