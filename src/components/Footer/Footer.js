b38d4dee7c1f684dffd9fe453beb7cfd984f02e8
chrisfromhood <kristijan@cogentdeciphering.com>
2021-11-05 09:16:43 +0100

    footer adjust

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,14 +15,16 @@ function Footer() {
                         </div>
                         <div className="col align-self-center">
                             <div className="right">
-                                <div className="footer__info-items place">
-                                    <i className="fa fa-location-arrow" aria-hidden="true"></i>
+                                <div className="info-items place">
+                                    <span className="icon location"><span className="fas fa-location-arrow"></span></span>
                                     <span className="text">
-                                        <span className="sub-text">Our Location </span> <br /> Local Service </span>
+                                        <span className="sub-text">Our Location</span> <br /> Local Service </span>
                                 </div>
-                                <div className="footer__info-items phone">
-                                    <span className="text">
-                                        <span className="sub-text">Call us now</span> <br /> 706-995-7485 </span>
+                                <div className="info-items phone">
+                                    <a href="tel:706-995-7485" className="phone">
+                                        <span className="icon"><span className="fas fa-phone-alt"></span> </span>
+                                        <span className="text"> <span className="sub-text">Call us now</span> <br /> 706-995-7485 </span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -32,7 +34,7 @@ function Footer() {
             <div className="footer-bottom">
                 <div className="container">
                     <span className="copyright">Copyright © 2021
-                        <a href="/" title="Jordan Air Quality Air &amp; Heating and Energy Solutions">Jordan Air Quality Air &amp; Heating and Energy Solutions</a>. All Rights Reserved.</span>
+                        <a href="/" title=" Jordan Air Quality Air &amp; Heating and Energy Solutions">Jordan Air Quality Air &amp; Heating and Energy Solutions</a>. All Rights Reserved.</span>
                 </div>
             </div>
         </footer>
